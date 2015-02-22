18bd81df50a8d95eb96bf96de3c1d76ce09ac3fd
root <root@fry.howdeshell.net>
2015-02-22 11:49:26 -0600

    adding crazyclock option

---
+++ crazyclock.sh
@@ -0,0 +1,16 @@
+for i in {1..10}
+do
+   /usr/local/bin/clock -R 255
+   sleep 1
+   /usr/local/bin/clock -B 255
+   sleep 1
+   /usr/local/bin/clock -G 255
+   sleep 1
+   /usr/local/bin/clock -P 255
+   sleep 1
+   /usr/local/bin/clock -Y 255
+   sleep 1
+   /usr/local/bin/clock -C 255
+   sleep 1
+
+done
--- install.sh
+++ install.sh
@@ -1,3 +1,5 @@
 #!/bin/bash
 cp ./RPi-ShiftBrite /usr/local/bin/clock
+cp ./crazyclock.sh /usr/local/bin/crazyclock
 chmod +s /usr/local/bin/clock
+
